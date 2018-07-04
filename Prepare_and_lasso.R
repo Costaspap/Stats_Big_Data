26304ec0a41cb5e85d2f62d46057e2385bfcfd48
Costaspap <costaspapastamos@hotmail.com>
2018-07-04 15:46:53 +0300

    Improvements
    Added logistic regression fit

--- Prepare_and_lasso.R
+++ Prepare_and_lasso.R
@@ -21,6 +21,12 @@ if (!require("glmnet")){
   library("glmnet")
   }
 
+if (!require("caret")){
+  install.packages("caret", dependencies=TRUE)
+  library("caret")
+  }else{
+  library("caret")
+  }
 
 data = data.frame(transpose(read.csv("alldata together.csv", header = FALSE, na.strings = 'NaN')))
 colnames(data) = trimws(data[1, ],which = 'right')
@@ -28,10 +34,11 @@ data = data[-1,]
 
 data$age = str_match(data[,1], "age ([0-9][0-9])")[,2]
 
+# Extract ERP info
 data$erp = str_match(data[,1], "erp (yes|no)")[,2]
 data$erp = ifelse(data$erp=="yes" & !is.na(data$erp), 1, 0)
 
-
+# Build target variable
 data$target = str_match(data[,1], ".5 yr survival")
 data$target = as.numeric(ifelse(data$target==">5 yr survival" , 1, 0))
 
@@ -42,9 +49,8 @@ data <- data.frame(lapply(data, function(x) {gsub(",", ".", x)}))
 
 
 data[] <- lapply(data, function(x) {
-  if(factor(x)) as.numeric(as.character(x)) else x
+  if(is.factor(x)) as.numeric(as.character(x)) else x
 })
-sapply(class,data)
 
 # Lazy replacement of NAs with column mean
 
@@ -57,16 +63,39 @@ for(i in 1:ncol(data)){
 na_cols = sapply(data, function(x) sum(is.na(as.numeric(as.character(x)))))
 full_na_cols = names(na_cols[na_cols == dim(data)[1]])
 
-# Prepare matrix
-ready_data = as.matrix(data[,!(names(data) %in% full_na_cols)])
+
+data = data[,!(names(data) %in% full_na_cols)]
+data_for_lasso = as.matrix(data)
 
 # Fit lasso
 
-lassoResults <- cv.glmnet(x =ready_data[,-24191], y=ready_data[,24191], 
-alpha=1, family="binomial")
+lassoResults <- cv.glmnet(x =data_for_lasso[,-24191], y=data_for_lasso[,24191], 
+alpha=1, family="binomial", intercept=FALSE)
 
-bestlambda<-lassoResults$lambda.min #lambda.lse can also be used.
+bestlambda<-lassoResults$lambda.1se
 
 results<-predict(lassoResults,s=bestlambda,type="coefficients")
 
 choicePred<-rownames(results)[which(results !=0)]
+
+# Logistic Regression Fitting
+
+set.seed('2018')
+
+# Split to train/test
+intrain = sample(seq(1,dim(data)[1],1),round(3/4 * dim(data)[1]),replace = FALSE)
+
+# Use only lasso's predictors
+train <- data[intrain , c(choicePred,'target')]
+test_X = data[-intrain , choicePred]
+test_Y = data[-intrain , 'target']
+
+# Build Logistic model
+model <- glm(target ~ .,family=binomial, data=train)
+
+predictions = ifelse(predict(model,test_X, type = 'response')>.5,1,0)
+
+# Summarize
+summary(model)
+
+confusionMatrix(factor(predictions),factor(test_Y))
